6012eb4ba88bee29b260cd3eb989f9e112880166
younglee <young.lee@ucb.edu.bo>
2022-09-05 17:11:05 -0400

    crear eventos

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("hola");
+
+palabrasDelUsuario = document.querySelector("#btcantidad");
+
+document.getElementById("btningreso").addEventListener("click", ingreso);
+document.getElementById("btngasto").addEventListener("click", gasto);
+
+
+const cantidadIngreso = document.querySelector("#inpcantidadi");
+const cantidadGasto = document.querySelector("#inpcantidadg");
+
+function ingreso() {
+    const valorCantidadIngreso = Number.parseInt(cantidadIngreso.value);
+    alert(valorCantidadIngreso);
+}
+function gasto() {
+    const valorcantidadGasto = Number.parseInt(cantidadGasto.value);
+    alert(valorcantidadGasto);
+}
